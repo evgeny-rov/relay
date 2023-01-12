5835c08d2e5bd01de1d28611375c35263020909e
Evgeny Rodionov <bitsinmyhead@gmail.com>
2023-01-12 16:09:07 +0800

    added session resumability

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,73 @@ import { Server } from 'socket.io';
 import crypto from 'crypto';
 import { getRandomHostId } from './uuid';
 
+interface Session {
+  token: string;
+  id: string;
+  active: boolean;
+}
+
 declare module 'socket.io' {
   interface Socket {
     userId: string;
-    hostId?: string;
+    session: Session;
   }
 }
 
-const PORT = Number(process.env.PORT ?? 3000);
+const sessions = new Map<string, Session>();
+
+const PORT = Number(process.env.PORT ?? 8080);
 const io = new Server(PORT, { cors: { origin: '*' } });
 
 io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  const session = sessions.get(token);
+
+  if (session) {
+    socket.session = session;
+    session.active = true;
+  }
+
   socket.userId = crypto.randomUUID();
   next();
 });
 
 io.on('connection', (socket) => {
-  socket.join(socket.userId);
-  socket.emit('session', { userId: socket.userId });
+  const session = socket.session;
 
-  socket.on('host', () => {
-    const hostId = getRandomHostId();
-    socket.hostId = hostId;
-
+  if (session) {
     socket.leave(socket.userId);
-    socket.join(hostId);
-    socket.emit('host:promoted', { hostId });
-  });
+    socket.join(socket.session.id);
+  } else {
+    socket.join(socket.userId);
+
+    socket.on('host', () => {
+      const session = { id: getRandomHostId(), token: crypto.randomUUID(), active: true };
+      socket.session = session;
+
+      sessions.set(session.token, session);
+
+      socket.leave(socket.userId);
+      socket.join(session.id);
+      socket.emit('host:promoted', { hostId: session.id, token: session.token });
+    });
+  }
 
   socket.on('peer', ({ type, to, data }) => {
     socket.to(to).emit('peer', { type, from: socket.userId, data });
   });
+
+  socket.on('disconnect', () => {
+    if (!socket.session) return;
+
+    socket.session.active = false;
+
+    // fire cleanup event after 10 minutes
+    setTimeout(() => {
+      if (socket.session.active) return;
+      sessions.delete(socket.session.token);
+    }, 1000 * 60 * 10);
+  });
 });
+
+console.log('started on port 8080');
